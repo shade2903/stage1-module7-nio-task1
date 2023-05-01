3690284b3bc8a56d39e4aa7382660bb16241a301
Vladimir Haiduk <nightshade2903@gmail.com>
2023-05-01 23:18:57 +0300

    completed

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,46 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile;
+        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file));) {
+            profile = getValueFromLine(reader);
+        } catch (IOException e) {
+            throw new MyCustomRuntimeException("File not found!");
+        }
+        return profile;
+    }
+
+    public Profile getValueFromLine(BufferedReader reader) throws IOException {
+        Profile profile = new Profile();
+        String line;
+        while ((line =  reader.readLine()) != null){
+            String [] values = line.split(":");
+            String key = values[0].trim();
+            String value = values[1].trim();
+            switch (key){
+                case ("Name"):
+                    profile.setName(value);
+                    break;
+                case ("Age"):
+                    profile.setAge(Integer.parseInt(value));
+                    break;
+                case ("Email"):
+                    profile.setEmail(value);
+                    break;
+                case ("Phone"):
+                    profile.setPhone(Long.parseLong(value));
+                    break;
+                default:
+                    break;
+            }
+        }
+        return profile;
     }
 }
---
+++ src/main/java/com/epam/mjc/nio/MyCustomRuntimeException.java
@@ -0,0 +1,7 @@
+package com.epam.mjc.nio;
+
+public class MyCustomRuntimeException extends RuntimeException{
+    public MyCustomRuntimeException(String message) {
+        super(message);
+    }
+}
